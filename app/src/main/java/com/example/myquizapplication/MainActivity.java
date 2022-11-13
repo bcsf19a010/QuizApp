28642b87302df462d078834c97122b1bfd60597a
Gohar Ali <Bcsf19a010@pucit.edu.pk>
2022-11-13 22:10:56 +0500

    done with front end

--- app/src/main/java/com/example/myquizapplication/MainActivity.java
+++ app/src/main/java/com/example/myquizapplication/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.myquizapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.snackbar.Snackbar;
@@ -7,6 +8,8 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.View;
+import android.R.id;
+import android.view.View.OnClickListener;
 
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -17,6 +20,8 @@ import com.example.myquizapplication.databinding.ActivityMainBinding;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,6 +29,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        Button btn_question;
+//        btn_question = findViewById(R.id.startQuestion);
+//
+//        btn_question.setOnClickListener(new View.OnClickListener(){
+//
+//            public void startQuiz(View view){
+//
+//        }
+//        });
+
 
 //        btnAdd =findViewById(R.id.buttonIncrement);
 //        textView = findViewById(R.id.textCount);
@@ -37,3 +53,5 @@ public class MainActivity extends AppCompatActivity {
 //        });
     }
 }
+
+
